fd7f3f918d5c47077ef16df87c37e98cf342bf22
Luke Warner <65521430+LukeWarnut@users.noreply.github.com>
2024-11-14 09:33:30 -0500

    Update INotificationServicesForApplication.cs

--- src/Ryujinx.HLE/HOS/Services/Notification/INotificationServicesForApplication.cs
+++ src/Ryujinx.HLE/HOS/Services/Notification/INotificationServicesForApplication.cs
@@ -3,6 +3,19 @@ namespace Ryujinx.HLE.HOS.Services.Notification
     [Service("notif:a")] // 9.0.0+
     class INotificationServicesForApplication : IpcService
     {
-        public INotificationServicesForApplication(ServiceCtx context) { }
+        private int AlarmSettingsCount;
+
+        public INotificationServicesForApplication(ServiceCtx context)
+        {
+            AlarmSettingsCount = 0;
+        }
+
+        [CommandCmif(520)]
+        public ResultCode ListAlarmSettings(ServiceCtx context)
+        {
+            context.ResponseData.Write(AlarmSettingsCount);
+
+            return ResultCode.Success;
+        }
     }
 }
