535efb5f49e831933fc156f903b7b33d2180292e
Luke Warner <65521430+LukeWarnut@users.noreply.github.com>
2024-11-15 09:55:05 -0500

    stuff

--- src/Ryujinx.HLE/HOS/Services/Ns/IApplicationManagerInterface.cs
+++ src/Ryujinx.HLE/HOS/Services/Ns/IApplicationManagerInterface.cs
@@ -1,5 +1,7 @@
 using LibHac.Ns;
 using Ryujinx.Common.Utilities;
+using System;
+using System.Linq;
 
 namespace Ryujinx.HLE.HOS.Services.Ns
 {
@@ -7,6 +9,27 @@ namespace Ryujinx.HLE.HOS.Services.Ns
     class IApplicationManagerInterface : IpcService
     {
         public IApplicationManagerInterface(ServiceCtx context) { }
+
+        [CommandCmif(0)]
+        // ListApplicationRecord(unknown<4>) -> (unknown<4>, buffer<unknown, 6>)
+        public ResultCode ListApplicationRecord(ServiceCtx context)
+        {
+            // input (unknown<4>)
+            string hex = "01008CF01BAAC00003020000000000000000000000000000";
+            // this returns 1322173
+            //int intValue = int.Parse(hex, System.Globalization.NumberStyles.HexNumber);
+
+            byte[] record = Enumerable.Range(0, hex.Length)
+                     .Where(x => x % 2 == 0)
+                     .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
+                     .ToArray();
+
+            //01008CF01BAAC000 EoW
+
+            context.Memory.Write(context.Request.ReceiveBuff[0].Position, record);
+
+            return ResultCode.Success;
+        }
 
         [CommandCmif(400)]
         // GetApplicationControlData(u8, u64) -> (unknown<4>, buffer<unknown, 6>)
